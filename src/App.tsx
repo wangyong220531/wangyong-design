88f669d2e03e9a5c7b131f2cdb1ed0d425bc8f28
AK-103 <apple@Apple.local>
2023-08-03 13:04:58 +0800

    Gallery componet update

--- src/App.tsx
+++ src/App.tsx
@@ -1,115 +1,114 @@
 import { FC } from "react"
-// import Gallery, { CardProps } from "./components/Gallery"
-import Search from "./components/Search"
+import Gallery, { CardProps } from "./components/Gallery"
 
 const App: FC = () => {
-    // const cards: CardProps[] = [
-    //     {
-    //         id: "000",
-    //         text: "A"
-    //     },
-    //     {
-    //         id: "001",
-    //         text: "B"
-    //     },
-    //     {
-    //         id: "002",
-    //         text: "C"
-    //     },
-    //     {
-    //         id: "003",
-    //         text: "D"
-    //     },
-    //     {
-    //         id: "004",
-    //         text: "E"
-    //     },
-    //     {
-    //         id: "005",
-    //         text: "F"
-    //     },
-    //     {
-    //         id: "006",
-    //         text: "G"
-    //     },
-    //     {
-    //         id: "007",
-    //         text: "H"
-    //     },
-    //     {
-    //         id: "008",
-    //         text: "I"
-    //     },
-    //     {
-    //         id: "009",
-    //         text: "J"
-    //     },
-    //     {
-    //         id: "010",
-    //         text: "K"
-    //     },
-    //     {
-    //         id: "011",
-    //         text: "L"
-    //     },
-    //     {
-    //         id: "012",
-    //         text: "M"
-    //     },
-    //     {
-    //         id: "013",
-    //         text: "N"
-    //     },
-    //     {
-    //         id: "014",
-    //         text: "O"
-    //     },
-    //     {
-    //         id: "015",
-    //         text: "P"
-    //     },
-    //     {
-    //         id: "016",
-    //         text: "Q"
-    //     },
-    //     {
-    //         id: "017",
-    //         text: "R"
-    //     },
-    //     {
-    //         id: "018",
-    //         text: "S"
-    //     },
-    //     {
-    //         id: "019",
-    //         text: "T"
-    //     },
-    //     {
-    //         id: "020",
-    //         text: "U"
-    //     },
-    //     {
-    //         id: "021",
-    //         text: "V"
-    //     },
-    //     {
-    //         id: "022",
-    //         text: "W"
-    //     },
-    //     {
-    //         id: "023",
-    //         text: "X"
-    //     },
-    //     {
-    //         id: "024",
-    //         text: "Y"
-    //     },
-    //     {
-    //         id: "025",
-    //         text: "Z"
-    //     }
-    // ]
-    return <Search/>
+    const cards: CardProps[] = [
+        {
+            id: "000",
+            text: "A"
+        },
+        {
+            id: "001",
+            text: "B"
+        },
+        {
+            id: "002",
+            text: "C"
+        },
+        {
+            id: "003",
+            text: "D"
+        },
+        {
+            id: "004",
+            text: "E"
+        },
+        {
+            id: "005",
+            text: "F"
+        },
+        {
+            id: "006",
+            text: "G"
+        },
+        {
+            id: "007",
+            text: "H"
+        },
+        {
+            id: "008",
+            text: "I"
+        },
+        {
+            id: "009",
+            text: "J"
+        },
+        {
+            id: "010",
+            text: "K"
+        },
+        {
+            id: "011",
+            text: "L"
+        },
+        {
+            id: "012",
+            text: "M"
+        },
+        {
+            id: "013",
+            text: "N"
+        },
+        {
+            id: "014",
+            text: "O"
+        },
+        {
+            id: "015",
+            text: "P"
+        },
+        {
+            id: "016",
+            text: "Q"
+        },
+        {
+            id: "017",
+            text: "R"
+        },
+        {
+            id: "018",
+            text: "S"
+        },
+        {
+            id: "019",
+            text: "T"
+        },
+        {
+            id: "020",
+            text: "U"
+        },
+        {
+            id: "021",
+            text: "V"
+        },
+        {
+            id: "022",
+            text: "W"
+        },
+        {
+            id: "023",
+            text: "X"
+        },
+        {
+            id: "024",
+            text: "Y"
+        },
+        {
+            id: "025",
+            text: "Z"
+        }
+    ]
+    return <Gallery cards={cards}/>
 }
 
 export default App
--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -7,7 +7,7 @@ export interface CardProps {
 
 const Card: FC<CardProps> = props => {
     const { text } = props
-    return <div className="w-[100px] h-[160px] bg-[#0735ff] text-[#fff] flex justify-center items-center">{text}</div>
+    return <div className="w-[100px] h-[160px] bg-blue-700 text-[#fff] flex justify-center items-center">{text}</div>
 }
 
 export interface GalleryProps {
@@ -15,29 +15,25 @@ export interface GalleryProps {
 }
 
 const Gallery: FC<GalleryProps> = props => {
-    const {cards} = props
+    const { cards } = props
     const [isSpecial, setIsSpecial] = useState(false)
 
     useEffect(() => {
-        const resizeHandler = () => {
-            const el = document.getElementById("box")
-            if (el) {
-                const x = Math.floor(parseFloat(getComputedStyle(el).width))
-                el.style.width = `${x}`
-                if ((x + 10) % 110 !== 0) {
-                    setIsSpecial(true)
-                } else {
-                    setIsSpecial(false)
-                }
+        const el = document.getElementById("box")
+        if (el) {
+            const x = Math.floor(parseFloat(getComputedStyle(el).width))
+            el.style.width = `${x}`
+            if ((x + 10) % 110 !== 0) {
+                setIsSpecial(true)
+            } else {
+                setIsSpecial(false)
             }
         }
-        window.addEventListener("resize", resizeHandler)
-        return () => window.removeEventListener("resize", resizeHandler)
     }, [])
 
     return (
         <div className="p-[40px]">
-            <div id="box" className="bg-blue-200 flex flex-wrap gap-[10px]" style={isSpecial ? { justifyContent: "space-between" } : {}}>
+            <div id="box" className="bg-blue-100 flex flex-wrap gap-[10px]" style={isSpecial ? { justifyContent: "space-between" } : {}}>
                 {cards.map(card => {
                     return <Card key={card.id} text={card.text} />
                 })}
